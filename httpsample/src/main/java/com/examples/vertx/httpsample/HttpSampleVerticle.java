b1ced31e765d9904b5671d0c7b04b0214ee66fec
gosuna78 <gilberto.osuna@outlook.com>
2019-07-21 12:47:17 -0500

    First commit

---
+++ httpsample/src/main/java/com/examples/vertx/httpsample/HttpSampleVerticle.java
@@ -0,0 +1,13 @@
+package com.examples.vertx.httpsample;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class HttpSampleVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> starter) {
+
+    }
+
+}
